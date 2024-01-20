63a494fa69fd94a9aa48cec1e702759535cc5849
Yara Azevedo <72475344+yara-azevedo@users.noreply.github.com>
2024-01-19 23:01:08 -0300

    A

--- App9_MinhasAnotaes/app/src/main/java/com/cursoandroid/app9_minhasanotaes/MainActivity.java
+++ App9_MinhasAnotaes/app/src/main/java/com/cursoandroid/app9_minhasanotaes/MainActivity.java
@@ -6,25 +6,36 @@ import android.os.Bundle;
 import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
     FloatingActionButton fob;
 
-    private SharedPreferencesss preferences;
-
+    Preferencias preferencias;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        preferences = new SharedPreferencess(getApplicationContext());
+        preferencias = new Preferencias(getApplicationContext());
         findA();
 
         fob.setOnClickListener(view -> {
-
+            String recuperado = editText.getText().toString();
+            if(recuperado.equals("")){
+                Snackbar.make(view, "nota vazia", Snackbar.LENGTH_LONG).show();
+            }else{
+                preferencias.salvar(recuperado);
+                Snackbar.make(view, "nota salva", Snackbar.LENGTH_LONG).show();
+            }
         });
+
+        String anotacao = preferencias.recuperar();
+        if(!anotacao.equals("")){
+            editText.setText(anotacao);
+        }
 
     }
 
--- App9_MinhasAnotaes/app/src/main/java/com/cursoandroid/app9_minhasanotaes/Preferencias.java
+++ App9_MinhasAnotaes/app/src/main/java/com/cursoandroid/app9_minhasanotaes/Preferencias.java
@@ -8,13 +8,23 @@ public class Preferencias {
     private Context context;
     private SharedPreferences preferences;
 
-    public void salvar(Context c
-    ){
-        this.context = c;
+    private SharedPreferences.Editor editor;
 
+    private final String NOME_ARQUIVO ="anotacao.preferencias";
+    private final String CHAVE_NOME = "";
+
+    public Preferencias(Context c) {
+        this.context = c;
+        preferences= context.getSharedPreferences(NOME_ARQUIVO, 0);
+        editor = preferences.edit();
     }
 
-    public void recuperar(){
+    public void salvar(String anotacao){
+        editor.putString(CHAVE_NOME,anotacao);
+        editor.commit();
+    }
 
+    public String recuperar(){
+        return preferences.getString(CHAVE_NOME, " ");
     }
 }
