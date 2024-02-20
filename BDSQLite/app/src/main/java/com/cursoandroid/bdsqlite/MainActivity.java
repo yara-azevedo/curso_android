f2195ec87d8ce412746cbe055a7202e4ff712658
Yara Azevedo <72475344+yara-azevedo@users.noreply.github.com>
2024-02-19 21:21:56 -0300

    A

--- BDSQLite/app/src/main/java/com/cursoandroid/bdsqlite/MainActivity.java
+++ BDSQLite/app/src/main/java/com/cursoandroid/bdsqlite/MainActivity.java
@@ -19,23 +19,34 @@ public class MainActivity extends AppCompatActivity {
             SQLiteDatabase sqLiteDatabase = openOrCreateDatabase("app", MODE_PRIVATE, null);
             //tabela
             sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS pessoa (" +
+                                                                       "ID INTEGER PRIMARY KEY AUTOINCREMENT," +
                                                                         "nome VARCHAR(20)," +
                                                                         "idade INT(2))");
+            sqLiteDatabase.execSQL("DROP TABLE pessoa");
             //inserir
             sqLiteDatabase.execSQL("INSERT INTO pessoa(nome, idade) VALUES ('Aaaa', 12)");
             sqLiteDatabase.execSQL("INSERT INTO pessoa(nome, idade) VALUES ('Bbbbb', 7)");
+            sqLiteDatabase.execSQL("INSERT INTO pessoa(nome, idade) VALUES ('ccc', 40)");
+            sqLiteDatabase.execSQL("INSERT INTO pessoa(nome, idade) VALUES ('ddd', 87)");
+
+            sqLiteDatabase.execSQL("UPDATE pessoa set idade = 18 where nome ='ddd'");
+            sqLiteDatabase.execSQL("DELETE FROM pessoa WHERE nome ='ddd'");
+
 
             //recuperar
-            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM pessoa", null);
+            String consulta= "SELECT * FROM pessoa WHERE 'Aaaa'";
 
+            Cursor cursor = sqLiteDatabase.rawQuery(consulta,  null);
+            int indiceID = cursor.getColumnIndex("id");
             int indice = cursor.getColumnIndex("nome");
             int indiceI = cursor.getColumnIndex("idade");
 
             cursor.moveToFirst();
 
             while(cursor != null){
-                Log.i("Resultado - nome: ", cursor.getString(indice));
-                Log.i("Resultado - idade: ", cursor.getString(indiceI));
+                String nome = cursor.getString(indice);
+                String idade = cursor.getString(indiceI);
+                Log.i("Resultado - nome: ",nome + " idade" +idade );
                 cursor.moveToNext();
             }
 
